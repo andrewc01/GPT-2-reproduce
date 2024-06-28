07f8ecf2358fd9f598482fed0bff5366272bafd8
andrewc01 <jhyeonc01@gmail.com>
2024-06-28 22:25:35 +0900

    added learning rate decay

--- train_gpt2.py
+++ train_gpt2.py
@@ -5,6 +5,9 @@ from torch.nn import functional as F
 import math
 
 # ------------------------------------------------------------------------------------------------
+# !wget https://raw.githubusercontent.com/karpathy/build-nanogpt/master/input.txt
+# ------------------------------------------------------------------------------------------------
+
 
 class CasualSelfAttention(nn.Module):
 
@@ -249,6 +252,23 @@ model.to(device)
 if device == 'cuda':
     model = torch.compile(model)
 
+max_lr = 3e-4
+min_lr = max_lr * 0.1
+warmup_steps = 10
+max_steps = 50
+def get_lr(it):
+    # 1) linear warmup for warmup_iters steps
+    if it < warmup_steps:
+        return max_lr * (it+1) / warmup_steps
+    # 2) if it > lr_decay_iters, return min learning rate
+    if it > max_steps:
+        return min_lr
+    # 3) in between, use cosine decay down to min learning rate
+    decay_ratio = (it - warmup_steps) / (max_steps - warmup_steps)
+    assert 0 <= decay_ratio <= 1
+    coeff = 0.5 * (1.0 + math.cos(math.pi * decay_ratio)) # coeff starts at 1 and goes to 0
+    return min_lr + coeff * (max_lr - min_lr)
+
 # optimize: 
 optimizer = torch.optim.AdamW(model.parameters(), lr=3e-4, betas=(0.9, 0.95), eps=1e-8)
 for i in range(50):
@@ -270,7 +290,7 @@ for i in range(50):
     t1 = time.time()
     dt = (t1 - t0)*1000
     tokens_per_sec = (train_loader.B * train_loader.T) / (t1 - t0)
-    print(f"step {i}, loss: {loss.item()}, dt: {dt:.2f}ms, tok/sec: {tokens_per_sec:.2f}")
+    print(f"step {i} | loss: {loss.item()} | norm: {norm:.4f} | dt: {dt:.2f}ms | tok/sec: {tokens_per_sec:.2f}")
 
 import sys; sys.exit(0)
 
