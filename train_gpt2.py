755e0273e305983f0bd675f2941404845457df0c
andrewc01 <jhyeonc01@gmail.com>
2024-06-28 15:37:28 +0900

    changed vocab_size to 50304 to make calculations more efficient.

--- train_gpt2.py
+++ train_gpt2.py
@@ -244,7 +244,7 @@ train_loader = DataLoaderLite(B=4, T=32)
 torch.set_float32_matmul_precision('high')
 
 # get logits
-model = GPT(GPTConfig())
+model = GPT(GPTConfig(vocab_size=50304))
 model.to(device)
 if device == 'cuda':
     model = torch.compile(model)
